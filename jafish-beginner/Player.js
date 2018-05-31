e842cc1ce8634ddb618c43e62cca359e48a2f073
DESKTOP-SUP6C2C\Administrator <>
2018-05-31 14:45:24 +0800

    9层完结，史诗模式为连续，该方法不适用

--- jafish-beginner/Player.js
+++ jafish-beginner/Player.js
@@ -68,21 +68,62 @@ const _units = [
 ]
 
 const __units = _units.map(item => {
-  let {target, ...unit} = item
+  let {
+    target,
+    ...unit
+  } = item
 
   unit = Object.assign(unit, target)
   unit.health = calculateHealth(unit.HP, unit.atk) // 对于该单位的安全血量
   unit.backHealth = calculateBackHealth(unit.HP, unit.atk) // 反向攻击的安全血量
-  unit.defeatCount = unit.atkDistance > 0 ? ceil(unit.HP / atk_3) : ceil(unit.HP / atk_5) // 击败单位需要行动的次数
+  unit.defeatAmount = unit.atkDistance > 0 ? ceil(unit.HP / atk_3) : ceil(unit.HP / atk_5) // 击败或解救单位需要行动的次数
 
   // 主动攻击单位
-  if (unit.atkDistance > unit.rely) {
+  if (unit.atkDistance > unit.rely && unit.rely !== 0) {
     // 攻击距离大于与上一个单位的距离则缩小攻击距离
     unit.atkDistance = unit.rely
   }
 
   return unit
 })
+
+const confirmes = [{ // 需要封装的api
+  name: 'walk', // api名称
+  options: { // 启用参数
+    currentEncounterType: 0, // 更新遭遇状态
+    defeatCount: false, // 当前单位进度+1
+  },
+}, {
+  name: 'feel',
+  options: {
+    currentEncounterType: 0,
+    defeatCount: false,
+  },
+}, {
+  name: 'rescue',
+  options: {
+    currentEncounterType: 2,
+    defeatCount: true,
+  },
+}, {
+  name: 'attack',
+  options: {
+    currentEncounterType: 1,
+    defeatCount: true,
+  },
+}, {
+  name: 'look',
+  options: {
+    currentEncounterType: 0,
+    defeatCount: false,
+  },
+}, {
+  name: 'shoot',
+  options: {
+    currentEncounterType: 1,
+    defeatCount: true,
+  },
+}]
 
 
 class Player {
@@ -97,6 +138,7 @@ class Player {
       runing: true, // 行进中
       retreat: false, // 战术撤退
       retreatDistanceCount: 0, // 撤退距离计数
+      defeatCount: 0, // 当前单位行动次数计数
     }
 
     // 初始化方向
@@ -105,8 +147,17 @@ class Player {
 
   playTurn(warrior) {
     // Cool code goes here.
-    const {units, currentEncounterType, runing, retreat} = this.state
-    let {unitCount, retreatDistanceCount, isForward} = this.state
+    const {
+      units,
+      currentEncounterType,
+      runing,
+      retreat
+    } = this.state
+    let {
+      unitCount,
+      retreatDistanceCount,
+      isForward
+    } = this.state
 
     // 方法封装
     warrior = this.confirmed(warrior)
@@ -114,14 +165,15 @@ class Player {
     const space = warrior._feel() // 感知区域，1个空格
     const spaceLook = warrior._look() // 感知区域，3个空格
 
-    // 在远程感知区域内有单位 0 -> 无单位， 1 -> 俘虏， 2 -> 敌人， 3 -> 其他
+    // 在远程感知区域内有单位 0 -> 无单位， 1 -> 俘虏， 2 -> 敌人， 3 -> 墙体， 4 -> 其他
     const unitList = spaceLook.map((item, index) => {
       if (item.isUnit()) {
         // 有单位
         const unit = item.getUnit()
         if (unit.isBound()) return [1, index]
         else if (unit.isEnemy()) return [2, index]
-        return [3, index]
+        else if (unit.isWall()) return [3, index]
+        return [4, index]
       } else {
         // 无单位
         return [0, index]
@@ -130,10 +182,7 @@ class Player {
     // 远程感知区域第一个单位
     const firstUnit = unitList.find(item => item[0] > 0)
 
-    if (space.isWall()) {
-      // 墙体则转身
-      warrior.pivot()
-    } else if (firstUnit) {
+    if (firstUnit) {
       // 远程感知内有单位
       switch (firstUnit[0]) {
         case 1:
@@ -143,12 +192,7 @@ class Player {
             warrior._walk() // 前进
           } else {
             // 在第一个位置 营救
-            this.updateCurrentEncounterType(2) // 变更遭遇状态
             warrior._rescue() // 营救
-            if (unitList.filter(item => (item[0] > 0) && (item[0] < 3)).length > 1) {
-              // 远程感知区域的单位至少2个
-              this.addUnitCount() // 遭遇单位计数+1
-            }
           }
           break
         case 2:
@@ -157,7 +201,6 @@ class Player {
             // 不在第一个位置
             if (units[unitCount].atkDistance > 0) {
               // 具有远程攻击的单位
-              this.updateCurrentEncounterType(1) // 变更遭遇状态
               warrior._shoot() // 射箭攻击
             } else {
               // 近战攻击单位
@@ -165,11 +208,14 @@ class Player {
             }
           } else {
             // 在第一个位置
-            this.updateCurrentEncounterType(1) // 变更遭遇状态
             warrior._attack() // 攻击
           }
           break
         case 3:
+          // 墙体
+          warrior.pivot() // 转身
+          break
+        case 4:
           // 其他
           warrior._walk() // 前进
           break
@@ -178,32 +224,10 @@ class Player {
           break
       }
 
-    }
-    /* } else if (space.isUnit()) {
-      // 有单位
-      this.updateRun(false) // 非行进中
-      const unit = space.getUnit() // 获取该单位
-
-      if (unit.isBound()) {
-        // 该单位是俘虏
-        this.updateCurrentEncounterType(2) // 变更遭遇状态
-        warrior._rescue() // 营救
-      } else if (unit.isEnemy()) {
-        // 该单位是敌人
-        this.updateCurrentEncounterType(1) // 变更遭遇状态
-        warrior._attack() // 攻击
-      } else {
-        // 其他单位
-        warrior._walk() // 前进
-      }
-    } */
-    else {
+    } else {
       // 无单位
       if (currentEncounterType !== 0) {
         // 类型转变
-        unitCount = this.addUnitCount() // 遭遇单位计数+1
-        isForward = this.verifyInversion() // 方向重置
-
         if (units[unitCount] && (units[unitCount].atkDistance >= units[unitCount].rely)) {
           // 在当前单位的攻击范围中
           this.updateRun(true) // 继续行进
@@ -217,7 +241,6 @@ class Player {
       }
       this.updateCurrentEncounterType(0) // 变更遭遇状态
 
-
       if (units[unitCount]) {
         const currentUnit = units[unitCount]
         const nextUnit = units[unitCount + 1]
@@ -227,7 +250,7 @@ class Player {
         // 额外安全生命值： 攻击距离 x 攻击力
         const extraHP = currentUnit.atkDistance * currentUnit.atk
         // 连续的额外安全生命值： 假设击败当前单位之后在下一个单位攻击范围内 ? 下个单位的安全生命 : 0
-        const nextExtraHP = nextUnit && nextUnit.atkDistance >= nextUnit.rely ? (((isForward ? nextUnit.health : nextUnit.backHealth) - 1) + (nextUnit.atk * nextUnit.atkDistance)) : 0
+        const nextExtraHP = nextUnit && nextUnit.atkDistance >= nextUnit.rely && nextUnit.defeatAmount > 1 ? (((isForward ? nextUnit.health : nextUnit.backHealth) - 1) + (nextUnit.atk * nextUnit.atkDistance)) : 0
 
         // 需要的生命值
         const needHP = basicHP + extraHP + nextExtraHP
@@ -279,6 +302,22 @@ class Player {
     }
   }
 
+  addDefeatCount() {
+    // 当前单位行动次数计数
+    this.state.defeatCount += 1
+
+    const {
+      defeatCount,
+      units,
+      unitCount
+    } = this.state
+
+    if (defeatCount >= units[unitCount].defeatAmount) {
+      // 达到需要行动次数
+      this.addUnitCount() // 遭遇单位进度+1
+      this.state.defeatCount = 0 // 重置计数
+    }
+  }
   addRetreatDistance(reset) {
     // 撤退距离计数
     if (reset === 'reset') {
@@ -301,7 +340,10 @@ class Player {
       return this.state.isForward = !this.state.isForward
     }
 
-    const {unitCount, units} = this.state
+    const {
+      unitCount,
+      units
+    } = this.state
 
     if (units[unitCount] && units[unitCount].inversion) {
       // 反向行动
@@ -314,16 +356,26 @@ class Player {
   confirmed(warrior) {
     // 行动方向封装
     // backward 向后  forward 向前
-    const {isForward} = this.state
-    const confirmes = ['walk', 'feel', 'rescue', 'attack', 'look', 'shoot'] // 需要封装的api
+    const {
+      isForward
+    } = this.state
 
-    confirmes.forEach(item => warrior[`_${item}`] = isForward ? warrior[item].bind(warrior) : warrior[item].bind(warrior, 'backward'))
+    confirmes.forEach(item => warrior[`_${item.name}`] = () => {
+      item.options.defeatCount && this.addDefeatCount() // 单位行动计数+1
+      item.options.currentEncounterType && this.updateCurrentEncounterType(item.options.currentEncounterType) // 变更遭遇状态
+
+      return isForward ? warrior[item.name].call(warrior) : warrior[item.name].call(warrior, 'backward')
+    })
 
     return warrior
   }
   addUnitCount() {
     // 遭遇单位计数进度+1
-    return ++this.state.unitCount
+    this.state.unitCount += 1
+
+    this.verifyInversion() // 方向重置
+
+    return this.state.unitCount
   }
   updateCurrentEncounterType(value) {
     // 改变当前遭遇状态
